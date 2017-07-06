61c32f069a654a232ae3e4f491842f1ad376bdca
Mike Gibson <mike.gibson@oneadvanced.com>
2017-07-06 17:16:20 +0100

    Add functionality to declare schema generation by comments

--- index.ts
+++ index.ts
@@ -2,7 +2,9 @@ import * as TJS from "typescript-json-schema";
 import * as globLib from "glob";
 import * as args from "commander";
 import * as ts from "typescript";
-import * as fs from "fs-promise";
+import * as fsPromise from "fs-promise";
+import * as fs from "fs";
+import * as readline from "readline";
 import * as path from "path";
 import * as mkdirp from "mkdirp";
 import * as chokidar from "chokidar";
@@ -15,8 +17,8 @@ args.version("0.1.10")
     .option("--srcRoot <path>", "Path to root directory with TypeScript files.") 
     .option("--srcFilePattern <glob>", "E.g. **/*.schema.ts - specifies which TypeScript files to process. This is relative to --srcRoot.") 
     .option("--targetRoot <path>", "Path to target directory where *.json schema files will be generated.") 
-    .option("--typeNamePrefix [prefix]", "Specifies type name prefix which is not included in file names.")
-    .option("--typeNameSuffix [suffix]", "Specifies type name suffix which is not included in file names.")
+    .option("--typeNamePrefix [prefix]", "Specifies type name prefix which is not included in file names, when comment-style markers are not included in a file.")
+    .option("--typeNameSuffix [suffix]", "Specifies type name suffix which is not included in file names, when comment-style markers are not included in a file.")
     .option("--defaultProps", "Create default properties definitions.", false) 
     .option("--required", "Create required array for non-optional properties.", false) 
     .option("--strictNullChecks", "Make values non-nullable by default.", false) 
@@ -95,13 +97,13 @@ async function generateManyFiles(sourceFilePaths: string[]): Promise<boolean> {
     let numberOfSuccessful = 0;
     for (let i = 0; i < sourceFilePaths.length; i++) {
         try {
-            await generateOneFile(sourceFilePaths[i], generator);
+            await generateForOneFile(sourceFilePaths[i], generator);
             numberOfSuccessful++;
             if (args["verbose"]) {
-                console.log(`Generated Json schema for ${sourceFilePaths[i]}.`);
+                console.log(`Generated Json schema(s) for ${sourceFilePaths[i]}.`);
             }
         } catch (error) {
-            console.error(`Failed to generate Json schema for ${sourceFilePaths[i]}: ${error}.`);
+            console.error(`Failed to generate Json schema(s) for ${sourceFilePaths[i]}: ${error}.`);
         }
     };
 
@@ -112,7 +114,22 @@ async function generateManyFiles(sourceFilePaths: string[]): Promise<boolean> {
     return numberOfSuccessful == sourceFilePaths.length;
 }
 
-async function generateOneFile(sourceFilePath: string, generator: TJS.JsonSchemaGenerator) {
+async function generateForOneFile(sourceFilePath: string, generator: TJS.JsonSchemaGenerator) {
+    const outputDirName = path.join(args["targetRoot"], path.relative(args["srcRoot"], path.dirname(sourceFilePath)));
+    mkdirp.sync(outputDirName);
+    
+    const typesDeclaredViaComments = await getTypesToGenerateDeclaredViaComments(sourceFilePath);
+    if (typesDeclaredViaComments.length > 0) {
+        for (let i = 0; i < typesDeclaredViaComments.length; i++) {
+            const typeDeclaredViaComments = typesDeclaredViaComments[i];
+            const schemaDefinition = generator.getSchemaForSymbol(typeDeclaredViaComments.typeName);
+            const outputFilePath = path.join(outputDirName, typeDeclaredViaComments.schemaFileName);
+            await fsPromise.writeJson(outputFilePath, schemaDefinition);
+        }
+
+        return;
+    }
+
     const sourceFileName = path.basename(sourceFilePath);
     const coreName = sourceFileName.split(".", 1)[0];
     
@@ -125,13 +142,62 @@ async function generateOneFile(sourceFilePath: string, generator: TJS.JsonSchema
     }
 
     const outputFileName = sourceFileName.slice(0, sourceFileName.lastIndexOf(".")) + ".json";
-    const outputDirName = path.join(args["targetRoot"], path.relative(args["srcRoot"], path.dirname(sourceFilePath)));
     const outputFilePath = path.join(outputDirName, outputFileName);
 
     const schemaDefinition = generator.getSchemaForSymbol(typeName);
 
-    mkdirp.sync(outputDirName);
-    await fs.writeJson(outputFilePath, schemaDefinition);
+    await fsPromise.writeJson(outputFilePath, schemaDefinition);
+}
+
+async function getTypesToGenerateDeclaredViaComments(sourceFilePath: string): Promise<{ typeName: string, schemaFileName: string }[]> {
+    return new Promise<{ typeName: string, schemaFileName: string }[]>((resolve, reject) => {
+        const parseState = { linesRead: 0, schemaFileName: "" };
+        const typesToGenerate: { typeName: string, schemaFileName: string }[] = [];
+        let rejectedPromise = false;
+
+        const commentMarkerRegEx = /generate-json-schema:([^ ]+)/i
+        const typeRegEx = /interface ([^ ]+)/i;
+
+        var rd = readline.createInterface({
+            input: fs.createReadStream(sourceFilePath),
+        });
+
+        rd.on("line", (line) => {
+            if (rejectedPromise) {
+                return;
+            }
+
+            parseState.linesRead++;
+
+            if (parseState.schemaFileName !== "") {
+                const typeMatch = typeRegEx.exec(line);
+                if (typeMatch === null) {
+                    reject(`Could not find type to generate schema for. Line ${parseState.linesRead} - '${line}'`);
+                    rejectedPromise = true;
+                    rd.close();
+                    return;
+                }
+
+                typesToGenerate.push({
+                    typeName: typeMatch[1],
+                    schemaFileName: parseState.schemaFileName,
+                });
+
+                parseState.schemaFileName = "";
+            } else {
+                const commentMatch = commentMarkerRegEx.exec(line);
+                if (commentMatch) {
+                    parseState.schemaFileName = commentMatch[1];
+                }
+            }
+        });
+
+        rd.on("close", () => {
+            if (!rejectedPromise) {
+                resolve(typesToGenerate);
+            }
+        })
+    });
 }
 
 function programFromConfig(fileNames: string[], configFileName: string): ts.Program {
